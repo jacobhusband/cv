8dce9c03f6efd518346da73e0ff7dcba4e643ea0
Jacob Husband <jakehusband2@gmail.com>
2022-01-22 10:08:38 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -5,13 +5,17 @@
 // Add a button to turn on the CPU then create an if statement that checks if that option is turned on or not.
 
 const tiles = document.querySelectorAll(".board-tile");
+const description = document.querySelector(".description");
 const playerOneImg = document.querySelector("#player-1-img");
 const playerTwoImg = document.querySelector("#player-2-img");
 const scorePlayerOne = document.querySelector("#player--1");
 const scorePlayerTwo = document.querySelector("#player--2");
 const cpu = document.querySelector("#cpu");
 const cpuScore = document.querySelector("#cpu-score");
-const tieScore = document.querySelector("#tie-score")
+const tieScore = document.querySelector("#tie-score");
+const winnerText = document.querySelector("#text");
+const overlay = document.querySelector("#overlay");
+const playAgain = document.querySelector("#rematch");
 
 const cpuTiles1 = [5, 6, 9, 10];
 
@@ -26,7 +30,6 @@ const win8 = [3, 7, 11, 15];
 const win9 = [0, 5, 10, 15];
 const win10 = [3, 6, 9, 12];
 
-let tileContainer = [];
 let winIndex = [];
 
 let offensiveDict = {};
@@ -36,8 +39,8 @@ winIndex.push(win1, win2, win3, win4, win5, win6, win7, win8, win9, win10);
 
 let playerOne = true;
 let bot = false;
-let newGame = false;
 let skipTheRest = false;
+let waitForNewGame = false;
 let clickCountOne = 0;
 let clickCountTwo = 0;
 let cpuCount = 0;
@@ -50,16 +53,24 @@ tiles.forEach((thing) => {
   thing.textContent = "";
 });
 
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+
 // console.log(`Click Count One: ${clickCountOne}`);
 // console.log(`Click Count Two: ${clickCountTwo}`);
 
 const newWinner = function () {
-  tiles.forEach((thing) => {
-    thing.textContent = "";
-  });
-
+  waitForNewGame = false;
+  playAgain.style.display = "none";
+  noTileContent();
+  removeTileBackgroundColor();
   playerOne = true;
-  newGame = true;
+  skipTheRest = false;
   playerOneImg.style.backgroundColor = "green";
   playerTwoImg.style.backgroundColor = "";
   clickCountOne = 0;
@@ -67,14 +78,54 @@ const newWinner = function () {
   cpuCount = 0;
   randomNumber = 0;
   square = "";
+};
 
+const noTileContent = function () {
+  tiles.forEach((thing) => {
+    thing.textContent = "";
+  });
+}
+
+const removeTileBackgroundColor = function () {
   tiles.forEach((thing) => {
     thing.style.backgroundColor = "";
   });
-};
+}
+
+const colorWinningTilesRed = function (index) {
+  winIndex[index].forEach((ind) => {
+    tiles[ind].style.backgroundColor = "red";
+  })
+}
+
+const makeOsBlue = function () {
+  tiles.forEach((thing) => {
+    if (thing.textContent === "O") thing.style.backgroundColor = "blue";
+  });
+}
+
+const makeXsBlue = function () {
+  tiles.forEach((thing) => {
+    if (thing.textContent === "X") thing.style.backgroundColor = "blue";
+  });
+}
+
+const winnerDisplay = function (winnerMessage, scoreTag, index) {
+  waitForNewGame = true;
+  console.log(winnerMessage);
+  scoreTag.textContent = Number(scoreTag.textContent) + 1;
+  winnerText.textContent = winnerMessage;
+  on();
+  playAgain.style.display = "block";
+  removeTileBackgroundColor();
+  if (index) {
+  colorWinningTilesRed(index);
+  }
+}
 
 const winnerCheck = function () {
   let count = 0;
+  let v;
   for (let x = 0; x < 10; x++) {
     let countOs = 0;
     let countXs = 0;
@@ -86,17 +137,15 @@ const winnerCheck = function () {
         countOs++;
       }
       if (countXs === 4) {
-        console.log("Player 1 has won the game!");
-        scorePlayerOne.textContent = Number(scorePlayerOne.textContent) + 1;
-        newWinner();
+        let msg = "Player 1 has won the game!";
+        winnerDisplay(msg, scorePlayerOne, x);
+        return 1;
       } else if (countOs === 4 && bot === false) {
-        console.log("Player 2 has won the game!");
-        scorePlayerTwo.textContent = Number(scorePlayerTwo.textContent) + 1;
-        newWinner();
+        let msg = "Player 2 has won the game!";
+        winnerDisplay(msg, scorePlayerTwo, x);
       } else if (countOs === 4 && bot === true) {
-        console.log("CPU has won the game!");
-        cpuScore.textContent = Number(cpuScore.textContent) + 1;
-        newWinner();
+        let msg = "CPU has won the game!";
+        winnerDisplay(msg, cpuScore, x);
       }
     }
   }
@@ -105,9 +154,8 @@ const winnerCheck = function () {
       count++;
     }
     if (count === 16) {
-      console.log(`Tie Game!`);
-      tieScore.textContent = Number(tieScore.textContent) + 1;
-      newWinner();
+      let msg = "Tie Game!";
+      winnerDisplay(msg, tieScore, v);
     }
   });
 };
@@ -230,53 +278,114 @@ const winConCheck = function () {
   }
 };
 
-const botCheck = function () {
-  // If the bot will not lose and has 2 O's in a win condition with the third position open, take it
-
+const populateArrayWithPossibleMoves = function (defensiveMoves) {
   let possibleMoveIndeces = [];
-  let offensiveMove = [];
-  let defensiveMove = [];
-  let nextMoveIndex;
-  let countXs = 0;
-  let countOs = 0;
-
-  for (let x = 0; x < 10; x++) {
-    countXs = 0;
-    countOs = 0;
-    for (let y = 0; y < 4; y++) {
-      let index = winIndex[x][y];
-      if (tiles[index].textContent === "X") {
-        countXs++;
-      } else if (tiles[index].textContent === "O") {
-        countOs++;
-      }
-    }
-    offensiveMove.push(countOs);
-    defensiveMove.push(countXs);
-  }
-
-  // Check if the bot has a way to win or is about to lose
-  if (!skipTheRest) {
-    winConCheck();
-    loseConCheck();
-  }
-
-  // If the bot cannot win and cannot lose make a player that is defensive
-  if (!skipTheRest) {
-    for (const [index, element] of defensiveMove.entries()) {
-      if (element > 0) {
-        for (let j = 0; j < element; j++) {
-          for (let i = 0; i < 4; i++) {
-            if (
-              tiles[winIndex[index][i]].textContent === "" ||
-              tiles[winIndex[index][i]].style.backgroundColor === "blue"
-            ) {
-              possibleMoveIndeces.push(winIndex[index][i]);
-            }
+  for (const [index, element] of defensiveMoves.entries()) {
+    if (element > 0) {
+      for (let j = 0; j < element; j++) {
+        for (let i = 0; i < 4; i++) {
+          if (
+            tiles[winIndex[index][i]].textContent === "" ||
+            tiles[winIndex[index][i]].style.backgroundColor === "blue"
+          ) {
+            possibleMoveIndeces.push(winIndex[index][i]);
           }
         }
       }
     }
+  }
+  return possibleMoveIndeces;
+}
+
+const countXsOrOs = function (letter) {
+  let moves = [];
+  let count = 0;
+
+  for (let x = 0; x < 10; x++) {
+    count = 0;
+    for (let y = 0; y < 4; y++) {
+      let index = winIndex[x][y];
+      if (tiles[index].textContent === letter) {
+        count++;
+      }
+    }
+    moves.push(count);
+  }
+  return moves;
+}
+
+const botAgressiveMove = function () {
+  let countOs;
+  let countOpenTiles;
+  let index;
+
+  for (let x = 0; x < 10; x++) {
+    countOs = 0;
+    countOpenTiles = 0;
+
+    for (let y = 0; y < 4; y++) {
+      index = winIndex[x][y];
+      console.log(index)
+      if (tiles[index].textContent === "O") {
+        countOs++;
+      } else if ((tiles[index].textContent === "X" && tiles[index].style.backgroundColor === "blue") || tiles[index].textContent === "") {
+        countOpenTiles++;
+      }
+    }
+
+    console.log(`The win index is: ${winIndex[x]}`)
+    console.log(`The count of Os, the goal is 2: ${countOs}`);
+    console.log(`The count of open tiles, goal is 2: ${countOpenTiles}`);
+
+    if (countOpenTiles >= 1 && countOs === 2) {
+      let clickCount = 0;
+      for (let y = 0; y < 4; y++) {
+        let index = winIndex[x][y];
+        if (tiles[index].textContent === "X" && tiles[index].style.backgroundColor === "blue" && clickCount === 0) {
+          tiles[index].textContent = "O";
+          skipTheRest = true;
+          clickCount++;
+          console.log(`The bot stole an X and got inside the first part of the loop.`)
+        } else if (tiles[index].textContent === "" && clickCount === 0) {
+          tiles[index].textContent = "O";
+          skipTheRest = true;
+          clickCount++;
+          console.log(`The bot got in an empty space and got inside the first part of the loop.`)
+        }
+      }
+    }
+    
+  }
+}
+
+const botCheck = function () {
+  // If the bot will not lose and has 2 O's in a win condition with the third position open, take it
+
+  let possibleMoveIndeces = [];
+  let defensiveMoves = [];
+  let nextMoveIndex;
+
+  // Check if the bot has a way to win or is about to lose
+  if (!skipTheRest) {
+    winConCheck();
+  }
+  if (!skipTheRest) {
+    loseConCheck();
+  }
+
+  // If the bot is not about to lose or win, check if there are two Os with either two empty spots next to it or an empty spot and a stealable X
+  // Add more console logs to the botAgressiveMove to see why it is not triggering
+  if (!skipTheRest) {
+    botAgressiveMove();
+    console.log(`Skip the rest: ${skipTheRest}`);
+  }
+
+  // If the bot cannot win and cannot lose make a player that is defensive
+  if (!skipTheRest) {
+    defensiveMoves = countXsOrOs("X");
+
+    // Mess with this to see if we can get the bot to play more aggressive when in the early stages of the game or not in danger of losing
+    possibleMoveIndeces = populateArrayWithPossibleMoves(defensiveMoves);
     nextMoveIndex = botFindNextMove(possibleMoveIndeces);
     tiles[nextMoveIndex].textContent = "O";
     console.log(
@@ -285,14 +394,22 @@ const botCheck = function () {
     winnerCheck();
   }
   if (cpuCount % 3 === 0) {
-    tiles.forEach((thing) => {
-      thing.style.backgroundColor = "";
-    });
-    tiles.forEach((thing) => {
-      if (thing.textContent === "O") thing.style.backgroundColor = "blue";
-    });
+    removeTileBackgroundColor();
+    makeOsBlue();
   }
 };
+
+const cpuRoundOneMove = function () {
+  let tileContainer = []
+  for (let x = 0; x < cpuTiles1.length; x++) {
+    square = tiles[cpuTiles1[x]];
+    if (square.textContent === "") {
+      tileContainer.push(square);
+    }
+  }
+  randomNumber = Math.trunc(Math.random() * tileContainer.length);
+  tileContainer[randomNumber].textContent = "O";
+}
 
 cpu.addEventListener("click", function () {
   if (bot === false) {
@@ -309,6 +426,8 @@ cpu.addEventListener("click", function () {
 
 document.querySelectorAll(".board-tile").forEach((item) => {
   item.addEventListener("click", (e) => {
+    if (!waitForNewGame){
+    let val = 0;
     // Change the tile from empty to X or blue O to X
     if (
       playerOne &&
@@ -318,76 +437,63 @@ document.querySelectorAll(".board-tile").forEach((item) => {
       if (bot === false) {
         playerTwoImg.style.backgroundColor = "green";
       }
+      
       clickCountOne++;
       item.textContent = "X";
-      winnerCheck();
-      if (!newGame) {
-        playerOne = false;
+      val = winnerCheck();
+      
+      playerOne = false;
+
+      if (!val) {
         skipTheRest = false;
+        
 
-        // Turn off blue for O's
-        tiles.forEach((thing) => {
-          thing.style.backgroundColor = "";
-        });
+        removeTileBackgroundColor();
 
-        // Color the X's blue every third action
         if (clickCountOne % 3 === 0) {
-          tiles.forEach((thing) => {
-            if (thing.textContent === "X") thing.style.backgroundColor = "blue";
-          });
+          makeXsBlue();
         }
+        
         if (bot === true) {
           playerOne = true;
           playerOneImg.style.backgroundColor = "green";
           cpuCount++;
           console.log(`The turn for the CPU is: ${cpuCount}`);
           if (cpuCount === 1) {
-            for (let x = 0; x < cpuTiles1.length; x++) {
-              square = tiles[cpuTiles1[x]];
-              if (square.textContent === "") {
-                tileContainer.push(square);
-              }
-            }
-            randomNumber = Math.trunc(Math.random() * tileContainer.length);
-            tileContainer[randomNumber].textContent = "O";
+            cpuRoundOneMove();
           } else {
             botCheck();
           }
         }
       }
+        
 
-      // Change the tile from empty to O or blue X to O
-    } else if (
-      !playerOne &&
-      (item.textContent === "" || item.style.backgroundColor === "blue") &&
-      bot === false
-    ) {
-      playerTwoImg.style.backgroundColor = "";
-      playerOneImg.style.backgroundColor = "green";
-      clickCountTwo++;
-      item.textContent = "O";
-      winnerCheck();
-      if (!newGame) {
-        playerOne = true;
+        // Change the tile from empty to O or blue X to O
+      } else if (
+        !playerOne &&
+        (item.textContent === "" || item.style.backgroundColor === "blue") &&
+        bot === false
+      ) {
+          playerTwoImg.style.backgroundColor = "";
+          playerOneImg.style.backgroundColor = "green";
+          clickCountTwo++;
+          item.textContent = "O";
+          winnerCheck();
+            playerOne = true;
 
-        // Turn off blue for X's
-        tiles.forEach((thing) => {
-          thing.style.backgroundColor = "";
-        });
+          // Turn off blue for X's
+          removeTileBackgroundColor();
 
-        // Color the O's blue every third action
-        if (clickCountTwo % 3 === 0) {
-          tiles.forEach((thing) => {
-            if (thing.textContent === "O") thing.style.backgroundColor = "blue";
-          });
+          // Color the O's blue every third action
+          if (clickCountTwo % 3 === 0) {
+            makeOsBlue();
+          }
         }
-      }
-    }
     // Need to check if someone won
-    if (!newGame) {
-      winnerCheck();
+    if (!val) {
+    winnerCheck();
     }
-    newGame = false;
+  }
   });
   // Add CPU logic
 });
